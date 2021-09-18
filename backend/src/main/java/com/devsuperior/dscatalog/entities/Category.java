084c539de6ba1140794ff6bd1e217b37459b1499
Lucas Moraes <lucasmoraes@alunos.utfpr.edu.br>
2021-09-18 08:32:05 -0300

    H2 database

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -1,11 +1,16 @@
 package com.devsuperior.dscatalog.entities;
 
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_category")
 public class Category implements Serializable
 {
     private static final long serialVersionUID = 667922986525075901L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
