d8472e3e295b0b01fbe35149f93b066f71ed3906
lucasrm <lucasmoraes@alunos.utfpr.edu.br>
2021-11-30 14:28:53 -0300

    ConstraintValidator customizado

--- backend/src/main/java/com/devsuperior/dscatalog/Dto/UserInsertDto.java
+++ backend/src/main/java/com/devsuperior/dscatalog/Dto/UserInsertDto.java
@@ -1,5 +1,8 @@
 package com.devsuperior.dscatalog.Dto;
 
+import com.devsuperior.dscatalog.services.validation.UserInsertValid;
+
+@UserInsertValid
 public class UserInsertDto extends UserDto {
 
     private String password;
